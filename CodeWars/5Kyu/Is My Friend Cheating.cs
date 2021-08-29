fcd2d1bb5527ea3f8d6b7bfb84219a09d604fd5b
JPHarford <62631816+JPHarford@users.noreply.github.com>
2021-08-29 18:22:04 -0500

    Create Is My Friend Cheating.cs

---
+++ CodeWars/5Kyu/Is My Friend Cheating.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class RemovedNumbers {
+  public static List<long[]> removNb(long n) {
+    
+    List<long[]> output = new List<long[]>();
+    
+    long gross = (n * (1+n))/2;
+    long solution;
+    
+    for(long i = 1; i <= n; i++)
+    {
+      if((gross - i)%(i + 1) != 0 || (gross - i)/(i + 1) > n) continue; 
+      
+      output.Add(new long[2] {i,(gross - i)/(i + 1)});
+    }
+      
+    return output;
+  }
+}
